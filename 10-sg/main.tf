7ae54521ac39895c8ff43ce67ddf9e728697477e
Govardhan Raju <gsurapur@gmail.com>
2025-11-30 17:40:11 +0530

    session-37

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,19 +1,19 @@
 # Using open source module
-# module "catalogue" {
-#     source ="terraform-aws-modules/security-group/aws"
+module "catalogue" {
+    source ="terraform-aws-modules/security-group/aws"
 
-#     name = "${local.common_name_sufix}-catalogue"
-#     use_name_prefix = false
-#     description = "Secirity group for catalogue with custom ports open within VPC, egress all traffic"
-#     vpc_id = data.aws_ssm_parameter.vpc_id.value
-# }
-
-module "sg" {
-    count = length(var.sg_names)
-    source = "git::https://github.com/sgovardhanraju/terraform-aws-sg.git?ref=main"
-    project_name = var.project_name
-    environment = var.environment
-    sg_name = var.sg_names[count.index]
-    sg_description = "Created for ${var.sg_names[count.index]}"
-    vpc_id = local.vpc_id
+    name = "${local.common_name_sufix}-catalogue"
+    use_name_prefix = false
+    description = "Secirity group for catalogue with custom ports open within VPC, egress all traffic"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
 }
+
+# module "sg" {
+#     count = length(var.sg_names)
+#     source = "git::https://github.com/sgovardhanraju/terraform-aws-sg.git?ref=main"
+#     project_name = var.project_name
+#     environment = var.environment
+#     sg_name = var.sg_names[count.index]
+#     sg_description = "Created for ${var.sg_names[count.index]}"
+#     vpc_id = local.vpc_id
+# }
