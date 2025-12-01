afee1d9157b89edde8bf7ec6b501e6ff4722eb7a
Govardhan Raju <gsurapur@gmail.com>
2025-12-01 22:25:09 +0530

    session-39

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "vpc_id" {
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project_name}/${var.environment}/backend_alb_sg_id"
 }
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+}
