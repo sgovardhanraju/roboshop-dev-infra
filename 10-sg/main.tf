5d23eadda67b011d1e7126e27623edafa867b5e2
Govardhan Raju <gsurapur@gmail.com>
2025-12-01 22:03:54 +0530

    session-39

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -17,3 +17,13 @@ module "sg" {
     sg_description = "Created for ${var.sg_names[count.index]}"
     vpc_id = local.vpc_id
 }
+
+# # Frontend accepting traffic from frontend ALB
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#     type = ingress
+#     security_group_id = module.sg[9].sg_id 
+#     source_security_group_id = module.sg[9].sg_id 
+#     from_port = 80
+#     protocol = "tcp"
+#     to_port = 80
+# }
