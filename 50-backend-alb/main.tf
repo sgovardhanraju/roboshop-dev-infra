c2cb69e4b82dbff0fa2c3842d0388555784988ad
Govardhan Raju <gsurapur@gmail.com>
2025-12-02 23:10:42 +0530

    session-40

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "backend_alb" {
     internal = false
     load_balancer_type = "application"
     security_groups = [local.backend_alb_sg_id]
-    subnets = local.public_subnet_ids
+    subnets = local.private_subnet_ids
 
     enable_deletion_protection = true # prevents accidental deletion from UI
 
