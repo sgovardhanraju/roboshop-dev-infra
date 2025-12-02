5661c37b1206233097ae5ca3bb439876da9c1d27
Govardhan Raju <gsurapur@gmail.com>
2025-12-02 23:10:16 +0530

    session-40

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -1,8 +1,17 @@
 resource "aws_security_group_rule" "backend_alb_bastion" {
-    type = ingress
+    type = "ingress"
     security_group_id = local.backend_alb_sg_id
     source_security_group_id = local.bastion_sg_id
     from_port = 80
     protocol = "tcp"
     to_port = 80
+}
+
+resource "aws_security_group_rule" "bastion-laptop" {
+    type = "ingress"
+    security_group_id = local.bastion_sg_id
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
 }
