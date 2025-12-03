d801faad34df7803c07199b0eb3c4b37652cbd31
Govardhan Raju <gsurapur@gmail.com>
2025-12-03 16:21:57 +0530

    session-40

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "mongodb" {
 }
 resource "terraform_data" "mongodb"{
     triggers_replace = [
-        aws_instance.mongo.id
+        aws_instance.mongodb.id
     ]
     connection {
         type = "ssh"
