c3aade306bd34e07e076b4d7fb5157c77f2f8573
Govardhan Raju <gsurapur@gmail.com>
2025-12-04 10:11:11 +0530

    session-41

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,14 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/sgovardhanraju/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
+#ansible-pull -U https://github.com/sgovardhanraju/ansible-roboshop-roles-tf.git -e component=$component main.yaml
+
+REPO_URP=https://github.com/sgovardhanraju/ansible-roboshop-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+mkdir -p $REPO_DIR
+mkdir -p /var/log/roboshop/
+touch ansible.log
+
