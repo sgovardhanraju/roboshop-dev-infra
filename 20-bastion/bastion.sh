315171672ad65c1eabae108f1aad895c6718deab
Govardhan Raju <gsurapur@gmail.com>
2025-12-05 13:13:59 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,10 +1,9 @@
-# copied from daws86s git repository
 #!/bin/bash
 
 # growing the /home volume for terraform purpose
-growpart /dev/nvme0n1 4
-lvextend -L +30G /dev/mapper/RootVG-homeVol
-xfs_growfs /home
+# growpart /dev/nvme0n1 4
+# lvextend -L +30G /dev/mapper/RootVG-homeVol
+# xfs_growfs /home
 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
@@ -13,9 +12,9 @@ sudo yum -y install terraform
 # sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
 
 # creating databases
-cd /home/ec2-user
-git clone https://github.com/sgovardhanraju/roboshop-dev-infra.git
-chown ec2-user:ec2-user -R roboshop-dev-infra
-cd roboshop-dev-infra/40-databases
-terraform init
-terraform apply -auto-approve
+# cd /home/ec2-user
+# git clone https://github.com/sgovardhanraju/roboshop-dev-infra.git
+# chown ec2-user:ec2-user -R roboshop-dev-infra
+# cd roboshop-dev-infra/40-databases
+# terraform init
+# terraform apply -auto-approve
