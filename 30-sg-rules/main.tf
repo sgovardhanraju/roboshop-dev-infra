6301555f5c488868bd08570df3efd073b11151be
Govardhan Raju <gsurapur@gmail.com>
2025-12-08 21:40:42 +0400

    session-41

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -54,14 +54,14 @@ resource "aws_security_group_rule" "redis_bastion" {
 # }
 
 # Created as part of ticket 1234GDF
-# resource "aws_security_group_rule" "mysql_bastion" {
-#   type              = "ingress"
-#   security_group_id = local.mysql_sg_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 22
-#   protocol          = "tcp"
-#   to_port           = 22
-# }
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  security_group_id = local.mysql_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
 
 # resource "aws_security_group_rule" "mysql_shipping" {
 #   type              = "ingress"
