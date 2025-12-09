f3685d3c01369ea13b6d537c33dfcaf2a7caeed7
Govardhan Raju <gsurapur@gmail.com>
2025-12-09 19:58:37 +0400

    session-42

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -140,13 +140,13 @@ resource "aws_autoscaling_group" "catalogue" {
   vpc_zone_identifier       = local.private_subnet_ids
   target_group_arns = [aws_lb_target_group.catalogue.arn]
 
-  instance_refresh {
-    strategy = "Rolling"
-    preferences {
-      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
-    }
-    triggers = ["launch_template"]
-  }
+  # instance_refresh {
+  #   strategy = "Rolling"
+  #   preferences {
+  #     min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+  #   }
+  #   triggers = ["launch_template"]
+  # }
   
   dynamic "tag" {  # we will get the iterator with name as tag
     for_each = merge(
