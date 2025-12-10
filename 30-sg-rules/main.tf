3b25821331dd6db85a1292df28bbf7eb2cd2c177
Govardhan Raju <gsurapur@gmail.com>
2025-12-10 19:51:08 +0400

    session-43

--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -206,14 +206,14 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
 # }
 
 # ##### Backend ALB SG Rules #####
-# resource "aws_security_group_rule" "backend_alb_bastion" {
-#   type              = "ingress"
-#   security_group_id = local.backend_alb_sg_id
-#   source_security_group_id = local.bastion_sg_id
-#   from_port         = 80
-#   protocol          = "tcp"
-#   to_port           = 80
-# }
+resource "aws_security_group_rule" "backend_alb_bastion" {
+  type              = "ingress"
+  security_group_id = local.backend_alb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+}
 
 # resource "aws_security_group_rule" "backend_alb_frontend" {
 #   type              = "ingress"
